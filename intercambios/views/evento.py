3b71fde4116734364bd7f36b16ef64dfdd19fa35
mchaparro <mx.mik3@gmail.com>
2013-11-16 11:10:39 -0700

    +seccion nuevo evento

--- intercambios/views/evento.py
+++ intercambios/views/evento.py
@@ -6,6 +6,16 @@ from django.contrib.auth import authenticate, login, logout
 from django.views.generic import UpdateView, CreateView
 
 def crear_evento(request):
-   
+    if request.method == 'POST':
+        
+        nombre_evento = request.POST['nombre_evento']
+        fecha = request.POST['fecha']
+        participantes = request.POST['participantes']
+        precio = request.POST['precio']
+        
+        evento = Evento(nombre=nombre_evento, precio=precio, participantes=participantes, fecha_evento=fecha, admin=request.user)
+        evento.save()
+        
+        return render_to_response('index.html', context_instance=RequestContext(request))
         
     return render_to_response('crear_evento.html', context_instance=RequestContext(request))

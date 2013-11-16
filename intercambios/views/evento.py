614967a4564e5df4c022f4e195a953ab237b62ad
mchaparro <mx.mik3@gmail.com>
2013-11-16 13:26:12 -0700

    Se agrego dependencia para formato de fechas 'pytz'

--- intercambios/views/evento.py
+++ intercambios/views/evento.py
@@ -1,19 +1,25 @@
 from django.http import *
 from django.shortcuts import render_to_response,redirect
 from django.template import RequestContext
-from intercambios.models import *
+from django.conf import settings
+from intercambios.models import Evento
 from django.contrib.auth import authenticate, login, logout
-from django.views.generic import UpdateView, CreateView
+import datetime
+import pytz
 
 def crear_evento(request):
     if request.method == 'POST':
+        local_TZ = pytz.timezone(settings.TIME_ZONE)
         
         nombre_evento = request.POST['nombre_evento']
         fecha = request.POST['fecha']
         participantes = request.POST['participantes']
         precio = request.POST['precio']
         
-        evento = Evento(nombre=nombre_evento, precio=precio, participantes=participantes, fecha_evento=fecha, admin=request.user)
+        fecha_evento = datetime.datetime.strptime(fecha, '%m/%d/%Y')
+        fecha_evento_TZ = local_TZ.localize(fecha_evento)
+        
+        evento = Evento(nombre=nombre_evento, precio=precio, participantes=participantes, fecha_evento=fecha_evento_TZ, admin=request.user)
         evento.save()
         
         return render_to_response('index.html', context_instance=RequestContext(request))

ccc34210c61f26cf121f2748580f68a2998a27a4
mchaparro <mx.mik3@gmail.com>
2013-11-17 11:29:26 -0700

    se agrego qtip2

--- intercambios/static/js/intercambios/crear_evento.js
+++ intercambios/static/js/intercambios/crear_evento.js
@@ -2,6 +2,32 @@ $(document).ready(function() {
 	   $('#fecha').datepicker({
 	    });
 
+	   //Validar la forma
+	   $('#crear_evento').click(function(e){
+		   e.preventDefault();
+		  //validar 
+		   if($('#nombre_evento').val().length > 20){
+			  
+			   if($('#nombre_evento').qtip('api'))  $('#nombre_evento').qtip('api').show();
+			   // No tooltip present, create it
+			   else
+			   {
+				   $('#nombre_evento').qtip(
+			      {
+			         content: 'Some basic content for the tooltip', 
+			         show: {
+			            when: 'click',
+			            ready: true
+			         }
+			      });
+			   }
+			   
+			   
+			   
+			   console.log('o nopis!!!!');		   
+		   
+		   }
+	   });
 });
 
 

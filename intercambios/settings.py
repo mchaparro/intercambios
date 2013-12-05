49a11df0ff3432266b4cc761200ef8f052520934
crackmoon <izizvanezza@hotmail.com>
2013-12-04 21:53:30 -0700

    setting correo heroku

--- intercambios/settings.py
+++ intercambios/settings.py
@@ -268,5 +268,11 @@ if(os.environ.get('DATABASE_URL')):
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
+    
+    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+    EMAIL_HOST= 'smtp.sendgrid.net'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
 
 
